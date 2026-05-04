7a068ce9635a9303645a2168364e5f4099ac2f8f
Mallikharjun <rapallimallikharjun218@gmail.com>
2026-05-04 09:27:43 +0530

    This is my first code

---
+++ dict.py
@@ -0,0 +1,19 @@
+Company = {
+
+    "emp1": {
+        "name": "Mallikharjun",
+        "age": 29,
+        "Company": "Infosys",
+        "Salary": 100000
+    },
+
+    "emp2": {
+        "name": "Arjun",
+        "age": 30,
+        "Company": "ACCENTURE",
+        "Salary": 200000
+    }
+}
+
+#print (Company["emp1"]["Company"][Salary])
+print (Company["emp2"]["Company"]["age"])
